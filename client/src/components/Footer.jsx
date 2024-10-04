c5d59474eb35d3be0c1c18bbbb4925c222213777
veraikhalo <ikhalo.oseaghe@gmail.com>
2024-10-04 14:59:55 +0100

    first commit

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Logo from "../img/logo.png";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p style={{fontWeight: 'bold', fontSize: 20}}>Vera diva</p>
+      <span>
+        Made with ♥️ and <b>React.js</b>.
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
